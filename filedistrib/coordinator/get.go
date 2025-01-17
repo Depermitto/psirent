4837b3709d19ec378ab37e909c57c6e12240e522
Paweł Wysocki <01178616@pw.edu.pl>
2025-01-17 14:21:28 +0100

    Added address limiting and order randomization

--- filedistrib/coordinator/get.go
+++ filedistrib/coordinator/get.go
@@ -2,13 +2,16 @@ package coordinator
 
 import (
 	"fmt"
-	"gitlab-stud.elka.pw.edu.pl/psi54/psirent/filedistrib/coms"
-	"gitlab-stud.elka.pw.edu.pl/psi54/psirent/filedistrib/persistent"
-	errors2 "gitlab-stud.elka.pw.edu.pl/psi54/psirent/internal/errors"
 	"io"
+	"math/rand/v2"
 	"net"
 	"strings"
 	"time"
+
+	"gitlab-stud.elka.pw.edu.pl/psi54/psirent/filedistrib/coms"
+	"gitlab-stud.elka.pw.edu.pl/psi54/psirent/filedistrib/persistent"
+	"gitlab-stud.elka.pw.edu.pl/psi54/psirent/internal/constants"
+	errors2 "gitlab-stud.elka.pw.edu.pl/psi54/psirent/internal/errors"
 )
 
 func Get(pw io.Writer, storage persistent.Storage, filehash string) error {
@@ -19,13 +22,22 @@ func Get(pw io.Writer, storage persistent.Storage, filehash string) error {
 	}
 
 	var validAddresses []string
-	addresses := storage[filehash]
+	// randomize the addres order for load balancing
+	addresses := make([]string, len(storage[filehash]))
+	perm := rand.Perm(len(addresses))
+	for i, v := range(perm) {
+		addresses[v] = storage[filehash][i]
+	}
 	for i := 0; i < len(addresses); i++ {
 		address := addresses[i]
 		d := net.Dialer{Timeout: 1 * time.Second} // timeout
 		if conn, err := d.Dial("tcp", address); err == nil {
 			if Has(conn, filehash) {
 				validAddresses = append(validAddresses, address)
+				// limit the number of addresses
+				if len(validAddresses) == constants.MAX_ADDR_NUM {
+					break
+				}
 			} else {
 				persistent.Remove(storage, filehash, i)
 			}
